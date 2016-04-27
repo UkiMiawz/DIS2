04812ae4df9135fee52730c258ad1dd024ce6793
Melisa Cecilia <melisa.cecilia@rwth-aachen.de>
2016-04-27 13:32:04 +0200

    Base code for window system

---
+++ 2 - WS Part 1/Code/WindowSystem.java
@@ -0,0 +1,14 @@
+import de.rwth.hci.Graphics.GraphicsEventSystem; 
+import java.io.Console;
+
+public class WindowSystem {
+
+	public WindowSystem(){
+		System.out.println("Constructor");
+	}
+
+	public static void main(String[] args){
+		WindowSystem windowSystem = new WindowSystem();
+		System.out.println("test");
+	}
+}
