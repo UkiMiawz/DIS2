f8a99e51046dad96b89b938c03ff0d6701268b35
Melisa Cecilia <melisa.cecilia@rwth-aachen.de>
2016-04-27 14:25:25 +0200

    add drawing and set color

--- 2 - WS Part 1/Code/WindowSystem.java
+++ 2 - WS Part 1/Code/WindowSystem.java
@@ -3,26 +3,51 @@ import java.awt.Color;
 
 public class WindowSystem extends GraphicsEventSystem{
 	
-	public WindowSystem(int i, int j){
-		super(i, j);
+	private int windowWidth;
+	private int windowHeight;
+	
+	/*
+	 * Constructor
+	 */
+	public WindowSystem(int width, int height){
+		//save window width and height
+		super(width, height);
+		windowWidth = width;
+		windowHeight = height;
 		handlePaint();
 	}
 
+	/*
+	 * Main class
+	 */
 	public static void main(String[] args){
 		WindowSystem windowSystem = new WindowSystem(500, 500);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see de.rwth.hci.Graphics.GraphicsEventSystem#handlePaint()
+	 */
 	protected void handlePaint(){
 		System.out.println("Drawing with Handle Paint");
-		drawLine(0.5,0.5,0.8,0.8);
+		this.drawLine(0.5f,0.5f,0.8f,0.8f);
 	}
 	
-	private void drawLine(float StartX, float StartY, float EndX, float EndY){
-		System.out.println("Draw line " + StartX + StartY + EndX + EndY);
-		super.drawLine(StartX, StartY, EndX, EndY);
-	}
-	 
-	public void setColor(Color inColor){
+	/*
+	 * Translate vector to coordinate
+	 * Draw line using parent draw line
+	 */
+	void drawLine(float startX, float startY, float endX, float endY){
+		System.out.println("Draw line with startx: " + startX + " starty: " + startY + 
+				" endx: " + endX + " endy " + endY);
+		double startWindowX = windowWidth * startX;
+		double startWindowY = windowHeight * startY;
+		double endWindowX = windowWidth * endX;
+		double endWindowY = windowHeight * endY;
 		
+		System.out.println("Draw line in coordinates: " + startWindowX + 
+				" starty: " + startWindowY + " endx: " + endWindowX + " endy " + endWindowY);
+		super.setColor(Color.BLACK);
+		super.drawLine((int)startWindowX, (int)startWindowY, (int)endWindowX, (int)endWindowY);
 	}
 }
