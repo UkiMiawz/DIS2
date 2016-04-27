fd21c89c1ec9b158a9f2e91a13913e6470abf2d3
Melisa Cecilia <melisa.cecilia@rwth-aachen.de>
2016-04-27 14:04:13 +0200

    update gitignore update calling base class

--- 2 - WS Part 1/Code/WindowSystem.java
+++ 2 - WS Part 1/Code/WindowSystem.java
@@ -1,14 +1,28 @@
 import de.rwth.hci.Graphics.GraphicsEventSystem; 
-import java.io.Console;
+import java.awt.Color;
 
-public class WindowSystem {
-
-	public WindowSystem(){
-		System.out.println("Constructor");
+public class WindowSystem extends GraphicsEventSystem{
+	
+	public WindowSystem(int i, int j){
+		super(i, j);
+		handlePaint();
 	}
 
 	public static void main(String[] args){
-		WindowSystem windowSystem = new WindowSystem();
-		System.out.println("test");
+		WindowSystem windowSystem = new WindowSystem(500, 500);
+	}
+
+	protected void handlePaint(){
+		System.out.println("Drawing with Handle Paint");
+		drawLine(0.5,0.5,0.8,0.8);
+	}
+	
+	private void drawLine(float StartX, float StartY, float EndX, float EndY){
+		System.out.println("Draw line " + StartX + StartY + EndX + EndY);
+		super.drawLine(StartX, StartY, EndX, EndY);
+	}
+	 
+	public void setColor(Color inColor){
+		
 	}
 }
