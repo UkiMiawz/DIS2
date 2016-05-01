360ce5ae2cecd658bad2aac8cb38d0fc0161ed5d
Melisa Cecilia <melisa.cecilia@yahoo.com>
2016-05-01 08:59:16 +0200

    Update SimpleWindow.java
    Add y min and max
    Add x min and max

--- 2 - WS Part 1/Code/SimpleWindow.java
+++ 2 - WS Part 1/Code/SimpleWindow.java
@@ -2,15 +2,19 @@ public class SimpleWindow{
 
 	private int indexValue;
 	private int zValue;
-	private int yValue;
-	private int xValue;
+	private int yMin;
+	private int xMin;
+	private int yMax;
+	private int xMax;
 
 	public SimpleWindow(){
 		
 	}
 
 	public int getIndex(){ return indexValue;} 
-	public int getX(){ return xValue; }
-	public int getY(){ return yValue; }
+	public int getXMin(){ return xMin; }
+	public int getYMin(){ return yMin; }
+	public int getYMax(){ return yMax; }
+	public int getXMax(){ return xMax; }
 	public int getZ(){ return zValue; }
 }
