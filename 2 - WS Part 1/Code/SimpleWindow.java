9f551605c6f987611f6dc86d29b8c8aff56c42e8
Melisa Cecilia <melisa.cecilia@yahoo.com>
2016-05-01 07:15:57 +0200

    Update SimpleWindow.java
    Add xyz to keep track of simple windows

--- 2 - WS Part 1/Code/SimpleWindow.java
+++ 2 - WS Part 1/Code/SimpleWindow.java
@@ -1,5 +1,14 @@
 public class SimpleWindow{
+
+	private int zValue;
+	private int yValue;
+	private int xValue;
+
 	public SimpleWindow(){
 		
 	}
+
+	public int getX(){ return xValue; }
+	public int getY(){ return yValue; }
+	public int getZ(){ return zValue; }
 }
