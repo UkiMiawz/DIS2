e90003e2dd16288f979a5b2c50c0a70460b271a6
Melisa Cecilia <melisa.cecilia@yahoo.com>
2016-05-01 07:24:47 +0200

    Update SimpleWindow.java
    Add index to simple window

--- 2 - WS Part 1/Code/SimpleWindow.java
+++ 2 - WS Part 1/Code/SimpleWindow.java
@@ -1,5 +1,6 @@
 public class SimpleWindow{
 
+	private int indexValue;
 	private int zValue;
 	private int yValue;
 	private int xValue;
@@ -8,6 +9,7 @@ public class SimpleWindow{
 		
 	}
 
+	public int getIndex(){ return indexValue;} 
 	public int getX(){ return xValue; }
 	public int getY(){ return yValue; }
 	public int getZ(){ return zValue; }
