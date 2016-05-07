556670cc3af1ae5a7e80ae8f353f9d1396cad1ea
Melisa Cecilia <melisa.cecilia.mail@gmail.com>
2016-05-07 22:13:22 +0200

    Cleaning up unused codes

--- assignment_3/src/RectangleComponent.java
+++ assignment_3/src/RectangleComponent.java
@@ -3,17 +3,18 @@ import java.awt.Color;
 
 
 public class RectangleComponent extends Rectangle{
+	
 	private Color myColor;
 	private String title;
+
 	public Color getColor(){ return myColor; }
-	public void setColor(Color value){ myColor = value; }
 	public String getString(){return title;}
+
+	public void setColor(Color value){ myColor = value; }
 	public void setString(String value){title = value;}
 
 	public RectangleComponent(int startX, int startY, int width, int height, Color myColor){
 		super(startX, startY, width, height);
 		this.myColor = myColor;
 	}
-
-	public void  drawTitle(String str, int x, int y) {}
 }
